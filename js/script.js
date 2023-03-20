a9ce98d2548edc48d65ed9d7912611fa2f135ec4
Ryanzhin22 <ryancpv2006@gmail.com>
2023-03-19 22:29:29 -0300

    segundo commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,49 @@
-var numerosAparecer = document.getElementById('numerosAparecer')
 var numeros = document.querySelector('.numeros')
-var select = document.querySelector('#quantidadeNum')
+var numerosLoto = document.querySelector('.numeros-loto')
+var numerosQuina = document.querySelector('.numeros-quina')
+var lotofacil = document.querySelector('.lotoFacil')
+var megasena = document.querySelector('.megaSena')
+var quina = document.querySelector('.quina')
 var numeroMax = 0
 
-function numMax(){
-    var valor = select.value
-    if(Number(valor) === 6){
-        numeroMax = 6
-    } if (Number(valor) === 7){
-        numeroMax = 7
-    } if (Number(valor) === 8){
-        numeroMax = 8
-    } if (Number(valor) === 9){
-        numeroMax = 9
-    } if (Number(valor) === 10){
-        numeroMax = 10
-    } if (Number(valor) === 11){
-        numeroMax = 11
-    } if (Number(valor) === 12){
-        numeroMax = 12
-    } if (Number(valor) === 13){
-        numeroMax = 13
-    } if (Number(valor) === 14){
-        numeroMax = 14
-    } if (Number(valor) === 15){
-        numeroMax = 15
-    }
+function lotoFacil(){
+    lotofacil.style.display = 'block'
+    megasena.style.display = 'none'
+    numeros.style.display = 'none'
+    quina.style.display = 'none'
+    numerosQuina.style.display = 'none'
 }
+
+function megaSena(){
+    megasena.style.display = 'block'
+    lotofacil.style.display = 'none'
+    numerosLoto.style.display = 'none'
+    quina.style.display = 'none'
+    numerosQuina.style.display = 'none'
+}
+
+function quiNa(){
+    quina.style.display = 'block'
+    lotofacil.style.display = 'none'
+    numerosLoto.style.display = 'none'
+    megasena.style.display = 'none'
+    numeros.style.display = 'none'
+}
+
+/* SORTEIO DOS NÚMEROS ALEATÓRIOS */
 
 function sorteio(){
     return parseInt(Math.random() * 60)
+}
+
+/*MEGASENA*/
+
+function numMax(){
+    var select = document.querySelector('#quantidadeNum')
+    var valor = select.value
+    for(var c = 0; valor >= c; c++){
+        numeroMax = c
+    }
 }
 
 function gerar(){
@@ -60,7 +74,76 @@ function gerar(){
                 valores.push(resultado)
             }
         }
-
     }
+    var numerosAparecer = document.getElementById('numerosAparecer')
     numerosAparecer.innerHTML = valores.sort()
+}
+
+/*LOTOFACIL*/
+
+function numMaxLoto(){
+    var selectLoto = document.querySelector('#quantidadeNumLoto')
+    var valor = selectLoto.value
+    for(var c = 0; valor >= c; c++){
+        numeroMax = c
+    }
+}
+
+function gerarLoto(){
+    valoresLoto = []
+    numerosLoto.style.display = 'block'
+
+    while (valoresLoto.length < numeroMax){
+        var resultadoLoto = sorteio()
+
+        if(valoresLoto.indexOf(resultadoLoto) === -1){
+            valoresLoto.push(resultadoLoto)
+        }
+    }
+
+    var aparecerLoto = document.getElementById('mudarLoto')
+    aparecerLoto.innerHTML = valoresLoto.sort()
+}
+
+/*QUINA*/
+
+function numMaxQuina(){
+    var select = document.querySelector('#quantidadeNumQuina')
+    var valor = select.value
+    for(var c = 0; valor >= c; c++){
+        numeroMax = c
+    }
+}
+
+function gerarQuina(){
+    valoresQuina = []
+    numerosQuina.style.display = 'block'
+
+    var imparParQuina = document.querySelector('input[name="imparOuParQuina"]:checked').value
+
+    while(valoresQuina.length < numeroMax){
+        var resultadoQuina = sorteio()
+
+        if(imparParQuina == 'impar'){
+            if(resultadoQuina % 2 == 1){
+                if(valoresQuina.indexOf(resultadoQuina) === -1){
+                    valoresQuina.push(resultadoQuina)
+                }
+            } 
+        }
+        if(imparParQuina == 'par'){
+            if(resultadoQuina % 2 == 0){
+                if(valoresQuina.indexOf(resultadoQuina) === -1){
+                    valoresQuina.push(resultadoQuina)
+                }
+            }
+        }
+        if(imparParQuina == 'parImpar'){
+            if(valoresQuina.indexOf(resultadoQuina) === -1){
+                valoresQuina.push(resultadoQuina)
+            }
+        }
+    }
+    var aparecerQuina = document.getElementById('mudarQuina')
+    aparecerQuina.innerHTML = valoresQuina.sort()
 }
